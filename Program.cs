05d0849e2e86cf13cba8e003489c47da5d157bf8
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-11-13 20:56:52 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace pripremaZaIspitZadatak7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("unesite neku rijec: ");
+            string rijec = Console.ReadLine();
+            if(rijec.Length < 5)
+            {
+                Console.WriteLine(rijec);
+
+            }
+            else
+            {
+                Console.WriteLine(rijec.Substring(2, rijec.Length - 4));
+            }
+            Console.ReadKey();
+        }
+    }
+}
